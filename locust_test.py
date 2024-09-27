40723e828b475f264722aae3dc9a236e9f33ce30
YooSeungJun <12000ha@naver.com>
2024-09-27 23:13:30 +0900

    가상환경제외 전체 커밋

--- locust_test.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, between
-
-class UserBehavior(HttpUser):
-    wait_time = between(1, 5) #  작업 사이 1초에서 5초 사이의 랜덤 대기 시간 지정
-
-    @task
-    def index(self):
-        self.client.get("/recaptcha") # 주소 url에 GET 요청 전송
